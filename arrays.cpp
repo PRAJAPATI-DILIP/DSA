ae9c145b63664b63a43739b5f098ce9cc13fad05
DILIP PRAJAPATI <dkprajapati9046@gmail.com>
2026-03-16 12:33:06 +0530

    array operation

---
+++ arrays.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int main(){
+
+     int array[]={1, 8, 6, 5, 2, 0, 3};
+     int i;
+     //array[0]=4;
+     for (i=0;i<=6;i++){
+        cout<<array[i] <<"\n";
+     }
+
+     //cout<<array;
+     return 0;
+
+ }
