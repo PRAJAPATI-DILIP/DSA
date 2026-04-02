f62d8714cb9d165d4415c1dc6fcd8590d219b5f1
DILIP PRAJAPATI <dkprajapati9046@gmail.com>
2026-04-02 12:56:43 +0530

    sorting operation

---
+++ bubble_sort.cpp
@@ -0,0 +1,46 @@
+// Time compexity o(n^2) ,so this is not a efficient algorithm 
+// Iteration upto (n-1)
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Bubble Sort
+    for(int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+
+        // Optimization: stop if already sorted
+        if(swapped == false) {
+            break;
+        }
+    }
+
+    // Print sorted array
+    cout << "Sorted array:\n";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
